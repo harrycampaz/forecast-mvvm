b437e1f9376ae103847372e01039d064dc722369
harrycampaz <harrycampaz@gmail.com>
2020-02-28 23:42:03 -0500

    first commit

---
+++ app/src/main/java/com/dezzapps/mrforecast/ui/setting/SettingsFragment.kt
@@ -0,0 +1,6 @@
+package com.dezzapps.mrforecast.ui.setting
+
+import androidx.fragment.app.Fragment
+
+class SettingsFragment : Fragment() {
+}
---
+++ app/src/main/java/com/dezzapps/mrforecast/ui/weather/current/CurrentWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.dezzapps.mrforecast.ui.weather.current
+
+import androidx.lifecycle.ViewModel
+
+class CurrentWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
